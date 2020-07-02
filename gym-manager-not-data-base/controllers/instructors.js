bcffeb1d9caf80ffe9e59ca3959a79581976b0d5
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-07-02 18:01:09 -0300

    using fs saving in JSON format, creation date, instructor ID and structuring data

--- gym-manager-not-data-base/controllers/instructors.js
+++ gym-manager-not-data-base/controllers/instructors.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const data = require('../file-system/data.json')
+
 // create
 exports.post = (req, res) => {
   //Validação todos os campos obrigatórios
@@ -8,5 +11,25 @@ exports.post = (req, res) => {
       return res.send("Por gentileza preencha todos os campos!")
   }
 
-  return res.send(req.body);
+  let { avatar_url, name, birth, gender, services } = req.body;
+
+  birth = Date.parse(birth);
+  const id = Number(data.instructors.length + 1);
+  const created_at = Date.now();
+
+  data.instructors.push({
+    id,  
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at});
+
+  fs.writeFile('file-system/data.json', JSON.stringify(data, null, 2), (err) => {
+      if(err) return res.send('Erro ao salvar o arquivo!')
+
+      return res.redirect('/instructors')
+  });
+
 }
