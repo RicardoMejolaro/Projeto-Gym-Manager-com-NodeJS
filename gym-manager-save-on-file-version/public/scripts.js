acf370bbdaee9ca376dcd1839631c6456d095c23
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-07-06 17:31:51 -0300

    include active menu logic

--- gym-manager-save-on-file-version/public/scripts.js
+++ gym-manager-save-on-file-version/public/scripts.js
@@ -1,11 +1,8 @@
-//selecionando todos os cards 
-const cards = document.querySelectorAll('.card');
-//Percorrendo os cards
-for (const card of cards) {
-  //Escutando o evento de click no card
-  card.addEventListener('click', () => {
-  //Pegando o atributo id do card 
-  const videoId = card.getAttribute('id');
-    window.location.href = `watch-video?id=${videoId}`;
-  });
+const currentPage = location.pathname;
+const menuItems = document.querySelectorAll('header .links a');
+
+for (const item of menuItems) {
+  if (currentPage.includes(item.getAttribute('href'))) {
+    item.classList.add("active")
+  }
 }
