14c080c5307bc96b036d7b5dbb6c2f87033aa9c0
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-07-06 20:45:47 -0300

    ready list of members on the index page

--- gym-manager-save-on-file-version/public/scripts.js
+++ gym-manager-save-on-file-version/public/scripts.js
@@ -1,3 +1,4 @@
+//Lógica link selecionado
 const currentPage = location.pathname;
 const menuItems = document.querySelectorAll('header .links a');
 
@@ -6,3 +7,15 @@ for (const item of menuItems) {
     item.classList.add("active")
   }
 }
+
+//Lógica confirmação de deletar
+const formDelete = document.querySelector("#form-delete");
+formDelete.addEventListener("submit", (event) => {
+  const confirmation = confirm("Deseja realmente deletar?");
+  if(!confirmation) {
+    event.preventDefault();
+  } else {
+    alert("Registro deletado com sucesso!");
+  }
+  
+});
