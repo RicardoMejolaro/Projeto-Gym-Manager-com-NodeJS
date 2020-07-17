6e7ec3d174370cb5cac58d475f1b8387c0e8f41a
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-07-17 17:37:04 -0300

    saving app data to the database

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,8 +1,9 @@
 const { age, date } = require('../../lib/utils');
+const db = require('../../config/db');
 
 module.exports = {
   index(req, res) {
-    return res.render('instructors/index', { instructors });
+    return res.render('instructors/index');
   },
   create(req, res) {
     return res.render('instructors/create');
@@ -16,9 +17,33 @@ module.exports = {
         return res.send("Por gentileza preencha todos os campos!")
     }
 
-    let { avatar_url, name, birth, gender, services } = req.body;
+    const query = `
+      INSERT INTO instructors (
+        avatar_url,
+        name,
+        birth,
+        gender,
+        services,
+        created_at
+      ) VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id
+    `
+     const { avatar_url, name, birth, gender, services} = req.body   
 
-    return
+    const values = [
+      avatar_url,
+      name,
+      date(birth).iso,
+      gender,
+      services,
+      date(Date.now()).iso
+    ];
+
+    db.query(query, values, (err, results) => {
+      if (err) return res.send('Erro no banco de dados!');
+
+      return res.redirect(`/instructors/${results.rows[0].id}`);
+    });
 
   },
   show(req, res) {
