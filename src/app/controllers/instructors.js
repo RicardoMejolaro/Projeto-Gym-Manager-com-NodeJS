3cc5c6d5d34ddfbefdaecd1bdbfbdfb008ca59e2
RicardoMejolaro <56407810+RicardoMejolaro@users.noreply.github.com>
2020-07-17 16:20:46 -0300

    changing controllers and utils for ES6 shorthand export method

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,136 +1,44 @@
-const fs = require('fs');
-const data = require('../file-system/data.json');
 const { age, date } = require('../../lib/utils');
 
-//Index
-exports.index = (req, res) => {
-  return res.render('instructors/index', { instructors: data.instructors });
-}
-//Create
-exports.create = (req, res) => {
-  return res.render('instructors/create'); 
-}
-//Post
-exports.post = (req, res) => {
-  //Validação todos os campos obrigatórios
-  const keys = Object.keys(req.body)
+module.exports = {
+  index(req, res) {
+    return res.render('instructors/index', { instructors });
+  },
+  create(req, res) {
+    return res.render('instructors/create');
+  },
+  post(req, res) {
+    //Validação todos os campos obrigatórios
+    const keys = Object.keys(req.body)
 
-  for (const key of keys) {
-    if (req.body[key] == "")
-      return res.send("Por gentileza preencha todos os campos!")
-  }
-
-  let { avatar_url, name, birth, gender, services } = req.body;
-
-  birth = Date.parse(birth);
-
-  let id = 1;
-  const lastMember = data.members[data.members.length - 1];
-
-  if (lastMember) {
-    id = Number(lastMember.id + 1);
-  }  
-  
-  const created_at = Date.now();
-
-  data.instructors.push({
-    id,
-    avatar_url,
-    name,
-    birth,
-    gender,
-    services,
-    created_at
-  });
-
-  fs.writeFile('file-system/data.json', JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send('Erro ao salvar o arquivo!')
-
-    return res.redirect(`/instructors/${id}`);
-  });
-
-}
-//Show
-exports.show = (req, res) => {
-  const { id } = req.params
-
-  const foundInstructor = data.instructors.find((instructor) => {
-    return instructor.id == id;
-  });
-
-  if (!foundInstructor) return res.send('Instrutor não encontrado!');
-
-  const instructor = {
-    ...foundInstructor,
-    age: age(foundInstructor.birth),
-    services: foundInstructor.services.split(','),
-    created_at: new Intl.DateTimeFormat("en-GB").format(foundInstructor.created_at)
-  }
-
-  return res.render('instructors/show', { instructor });
-
-}
-//Edit
-exports.edit = (req, res) => {
-  const { id } = req.params
-
-  const foundInstructor = data.instructors.find((instructor) => {
-    return instructor.id == id;
-  });
-
-  if (!foundInstructor) return res.send('Instrutor não encontrado!');
-
-  const instructor = {
-    ...foundInstructor,
-    birth: date(foundInstructor.birth).iso
-  }
-
-  return res.render('instructors/edit', { instructor });
-}
-//Put
-exports.put = (req, res) => {
-  const { id } = req.body;
-  let index = 0;
-
-  const foundInstructor = data.instructors.find((instructor, foundIndex) => {
-    if (instructor.id == id) {
-      index = foundIndex;
-
-      return true
+    for (const key of keys) {
+      if (req.body[key] == "")
+        return res.send("Por gentileza preencha todos os campos!")
     }
-  });
 
-  if (!foundInstructor) return res.send('Instrutor não encontrado!');
+    let { avatar_url, name, birth, gender, services } = req.body;
 
-  const instructor = {
-    ...foundInstructor,
-    ...req.body,
-    id: Number(req.body.id),
-    birth: Date.parse(req.body.birth)
-  }
+    return
 
-  data.instructors[index] = instructor;
+  },
+  show(req, res) {
+    return
+  },
+  edit(req, res) {
+    return
+  },
+  put(req, res) {
+    //Validação todos os campos obrigatórios
+    const keys = Object.keys(req.body)
 
-  fs.writeFile('file-system/data.json', JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send('Erro ao salvar dados no arquivo!');
-
-    return res.redirect(`/instructors/${id}`);
-  });
+    for (const key of keys) {
+      if (req.body[key] == "")
+        return res.send("Por gentileza preencha todos os campos!")
+    }
+    return
+  },
+  delete(req, res) {
+    return
+  },
 }
-//Delete
-exports.delete = (req, res) => {
-  const { id } = req.body
 
-  const filteredInstructors = data.instructors.filter((instructor) => {
-    return instructor.id != id;
-  });
-
-  data.instructors = filteredInstructors;
-
-  fs.writeFile('file-system/data.json', JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send('Erro ao salvar dados no arquivo!');
-
-    return res.redirect('/instructors');
-
-  });
-}
--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,134 +1,43 @@
-const fs = require('fs');
-const data = require('../file-system/data.json');
-const { date } = require('../../lib/utils');
+const { age, date } = require('../../lib/utils');
 
-//Index
-exports.index = (req, res) => {
-  return res.render('members/index', { members: data.members });
-}
-//Create
-exports.create = (req, res) => {
-  return res.render('members/create'); 
-}
-//Post
-exports.post = (req, res) => {
-  //Validação todos os campos obrigatórios
-  const keys = Object.keys(req.body)
+module.exports = {
+  index(req, res) {
+    return res.render('members/index', { members });
+  },
+  create(req, res) {
+    return res.render('members/create');
+  },
+  post(req, res) {
+    //Validação todos os campos obrigatórios
+    const keys = Object.keys(req.body)
 
-  for (const key of keys) {
-    if (req.body[key] == "")
-      return res.send("Por gentileza preencha todos os campos!")
-  }
-
-  let { avatar_url, name, email, birth, gender, blood, weight, height } = req.body;
-
-  birth = Date.parse(birth);
-
-  let id = 1;
-  const lastMember = data.members[data.members.length - 1];
-
-  if (lastMember) {
-    id = Number(lastMember.id + 1);
-  }  
-
-  data.members.push({
-    id,
-    avatar_url,
-    name,
-    email,
-    birth,
-    gender,
-    blood,
-    weight,
-    height
-  });
-
-  fs.writeFile('file-system/data.json', JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send('Erro ao salvar o arquivo!');
-
-    return res.redirect(`/members/${id}`);
-  });
-
-}
-//Show
-exports.show = (req, res) => {
-  const { id } = req.params
-
-  const foundMember = data.members.find((member) => {
-    return member.id == id;
-  });
-
-  if (!foundMember) return res.send('Instrutor não encontrado!');
-
-  const member = {
-    ...foundMember,
-    birth: date(foundMember.birth).birthDay
-  }
-
-  return res.render('members/show', { member });
-
-}
-//Edit
-exports.edit = (req, res) => {
-  const { id } = req.params
-
-  const foundMember = data.members.find((member) => {
-    return member.id == id;
-  });
-
-  if (!foundMember) return res.send('Instrutor não encontrado!');
-
-  const member = {
-    ...foundMember,
-    birth: date(foundMember.birth).iso
-  }
-
-  return res.render('members/edit', { member });
-}
-//Put
-exports.put = (req, res) => {
-  const { id } = req.body;
-  let index = 0;
-
-  const foundMember = data.members.find((member, foundIndex) => {
-    if (member.id == id) {
-      index = foundIndex;
-
-      return true
+    for (const key of keys) {
+      if (req.body[key] == "")
+        return res.send("Por gentileza preencha todos os campos!")
     }
-  });
 
-  if (!foundMember) return res.send('Instrutor não encontrado!');
+    let { avatar_url, name, birth, gender, services } = req.body;
 
-  const member = {
-    ...foundMember,
-    ...req.body,
-    id: Number(req.body.id),
-    birth: Date.parse(req.body.birth)
-  }
+    return
 
-  data.members[index] = member;
+  },
+  show(req, res) {
+    return
+  },
+  edit(req, res) {
+    return
+  },
+  put(req, res) {
+    //Validação todos os campos obrigatórios
+    const keys = Object.keys(req.body)
 
-  fs.writeFile('file-system/data.json', JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send('Erro ao salvar dados no arquivo!');
-
-    return res.redirect(`/members/${id}`);
-  });
-}
-//Delete
-exports.delete = (req, res) => {
-  const { id } = req.body
-
-  const filteredMembers = data.members.filter((member) => {
-    return member.id != id;
-  });
-
-  data.members = filteredMembers;
-
-  fs.writeFile('file-system/data.json', JSON.stringify(data, null, 2), (err) => {
-    if (err) return res.send('Erro ao salvar dados no arquivo!');
-
-    return res.redirect('/members');
-
-  });
+    for (const key of keys) {
+      if (req.body[key] == "")
+        return res.send("Por gentileza preencha todos os campos!")
+    }
+    return
+  },
+  delete(req, res) {
+    return
+  },
 }
